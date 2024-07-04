5c1f80c6b8301d90406cb42373255679f2daf12e
Miguel Fernando Marlés Betancourt <ingenieromarles@gmail.com>
2024-07-03 21:53:58 -0500

    Agregamos botón a pantalla principal

--- app/src/main/java/com/example/ejemplo_github/MainActivity.java
+++ app/src/main/java/com/example/ejemplo_github/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ejemplo_github;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +14,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Log.e("msg", "");
+
     }
 }
